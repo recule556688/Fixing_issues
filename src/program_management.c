256dfb95b6a7d5a9e3cfec6efbaca60719218f82
Iuliia Dabizha <iuliia.dabizha@epitech.eu>
2025-06-05 16:15:10 +0200

    [FIX] fixing coding style

--- src/program_management.c
+++ src/program_management.c
@@ -46,37 +46,49 @@ static void set_program_header(program_t *program, header_t *header)
     program->header = *header;
 }
 
+static int validate_load_address(int load_addr, unsigned int prog_size)
+{
+    if (load_addr < 0 || load_addr >= MEM_SIZE) {
+        my_printf("Error: Invalid load address: %d\n", load_addr);
+        return 84;
+    }
+    if (load_addr > MEM_SIZE - prog_size) {
+        my_printf("Error: Program would exceed memory bounds"
+            " (addr %d + size %u > %d)\n",
+            load_addr, prog_size, MEM_SIZE);
+        return 84;
+    }
+    return 0;
+}
+
+static int validate_program_load(unsigned int prog_size, int load_addr)
+{
+    my_printf("Debug: LPTM entry: address_param=%d,"
+        " local_load_addr=%d, prog_size_param=%u, local_prog_size=%u\n",
+        load_addr, load_addr, prog_size, prog_size);
+    my_printf("Debug: Loading program of size %u at address %d\n",
+        prog_size, load_addr);
+    if (prog_size == 0) {
+        my_printf("Error: Invalid program size in LPTM: %u\n",
+            prog_size);
+        return 84;
+    }
+    if (prog_size > MEM_SIZE) {
+        my_printf("Error: Program size %u exceeds memory size %d\n",
+            prog_size, MEM_SIZE);
+        return 84;
+    }
+    return validate_load_address(load_addr, prog_size);
+}
+
 void load_program_to_memory(vm_t *vm, program_t *program,
     char *program_bytes, int address_param, unsigned int prog_size_param)
 {
     int local_load_addr = address_param;
     unsigned int local_prog_size = prog_size_param;
 
-    my_printf("Debug: LPTM entry: address_param=%d,"
-        " local_load_addr=%d, prog_size_param=%u, local_prog_size=%u\n",
-        address_param, local_load_addr, prog_size_param, local_prog_size);
-    my_printf("Debug: Loading program of size %u at address %d\n",
-        local_prog_size, local_load_addr);
-    if (local_prog_size == 0) {
-        my_printf("Error: Invalid program size in LPTM: %u\n",
-            local_prog_size);
+    if (validate_program_load(local_prog_size, local_load_addr) != 0)
         return;
-    }
-    if (local_prog_size > MEM_SIZE) {
-        my_printf("Error: Program size %u exceeds memory size %d\n",
-            local_prog_size, MEM_SIZE);
-        return;
-    }
-    if (local_load_addr < 0 || local_load_addr >= MEM_SIZE) {
-        my_printf("Error: Invalid load address: %d\n", local_load_addr);
-        return;
-    }
-    if (local_load_addr > MEM_SIZE - local_prog_size) {
-        my_printf("Error: Program would exceed memory bounds"
-            " (addr %d + size %u > %d)\n",
-            local_load_addr, local_prog_size, MEM_SIZE);
-        return;
-    }
     my_printf("Debug: Copying %u bytes to memory at address %d\n",
         local_prog_size, local_load_addr);
     for (unsigned int i = 0; i < local_prog_size; i++) {
@@ -86,6 +98,20 @@ void load_program_to_memory(vm_t *vm, program_t *program,
     my_printf("Debug: Set program PC to %d\n", program->pc);
 }
 
+static void print_program_debug_info(int address, int prog_number,
+    header_t *header_param, unsigned int confirmed_prog_size)
+{
+    my_printf("Debug: create_program received address: %d,"
+        " prog_number: %d, header_param->prog_size(raw): %d\n",
+        address, prog_number, header_param->prog_size);
+    my_printf("Debug: create_program after set_program_header,"
+        " program->header.prog_size: %u, captured confirmed_prog_size: %u\n",
+        header_param->prog_size, confirmed_prog_size);
+    my_printf("Debug: create_program calling LPTM with address: %d,"
+        " confirmed_prog_size: %u\n",
+        address, confirmed_prog_size);
+}
+
 program_t *create_program(vm_t *vm, char *program_bytes,
     header_t *header_param, int adress, int prog_number)
 {
@@ -93,9 +119,8 @@ program_t *create_program(vm_t *vm, char *program_bytes,
     int confirmed_address = adress;
     unsigned int confirmed_prog_size = 0;
 
-    my_printf("Debug: create_program received address: %d,"
-        " prog_number: %d, header_param->prog_size(raw): %d\n",
-        confirmed_address, prog_number, header_param->prog_size);
+    print_program_debug_info(confirmed_address, prog_number,
+        header_param, confirmed_prog_size);
     program = initialize_program(confirmed_address, prog_number);
     if (!program) {
         my_printf("Error: initialize_program failed in create_program\n");
@@ -103,12 +128,6 @@ program_t *create_program(vm_t *vm, char *program_bytes,
     }
     set_program_header(program, header_param);
     confirmed_prog_size = program->header.prog_size;
-    my_printf("Debug: create_program after set_program_header,"
-        " program->header.prog_size: %u, captured confirmed_prog_size: %u\n",
-        program->header.prog_size, confirmed_prog_size);
-    my_printf("Debug: create_program calling LPTM with address: %d,"
-        " confirmed_prog_size: %u\n",
-        confirmed_address, confirmed_prog_size);
     load_program_to_memory(vm, program, program_bytes,
         confirmed_address, confirmed_prog_size);
     return program;
